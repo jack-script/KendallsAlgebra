58404b5d82cd360727761e5b8cf4b878de020863
danielromeo <danielromeo99@gmail.com>
2021-11-19 15:52:19 +0200

    first commit

---
+++ kendallsalgebra.cpp
@@ -0,0 +1,11 @@
+#include "kendallsalgebra.h"
+#include <iostream>
+
+KendallsAlgebra::KendallsAlgebra()
+{
+
+}
+
+void KendallsAlgebra::printMatrix(){
+    std::cout << "hello kendall";
+}
---
+++ kendallsalgebra.h
@@ -0,0 +1,11 @@
+#ifndef KENDALLSALGEBRA_H
+#define KENDALLSALGEBRA_H
+
+class KendallsAlgebra
+{
+public:
+    KendallsAlgebra();
+    void printMatrix();
+};
+
+#endif // KENDALLSALGEBRA_H
