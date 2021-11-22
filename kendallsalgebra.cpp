e5cb85723fac9c25ae722f68f6c84a1ea7dba934
danielromeo <danielromeo99@gmail.com>
2021-11-22 22:33:56 +0200

    added generate random matrix with proper formatting

--- kendallsalgebra.cpp
+++ kendallsalgebra.cpp
@@ -1,11 +1,46 @@
 #include "kendallsalgebra.h"
 #include <iostream>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+#include <vector>
+
 KendallsAlgebra::KendallsAlgebra()
 {
 
 }
 
 void KendallsAlgebra::generateAndPrintRandomMatrix(){
-    std::cout << "hello kendall";
+
+    int size = 3;
+    srand(time(0));
+
+    for(int i = 0; i < size; i++){
+        std::vector<int> innervec;
+
+        for(int j = 0 ; j < size; j++){
+            innervec.push_back(rand()% 100 + 1);
+        }
+        vec.push_back(innervec);
+        // innervec.empty();
+    }
+    // std::cout << vec[0]<< std::endl;
+    // print
+    print2dVector(vec);
 }
+
+void KendallsAlgebra::print2dVector(std::vector<std::vector<int>>){
+    for(int i = 0; i < vec.size(); ++i){
+             std::cout << "\t";
+            // std::cout << "-----" << std::endl;
+            for(int j = 0; j < vec[i].size(); ++j){
+
+                std::cout  << vec[i][j] << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "\t" << "-- -- --" << std::endl;
+        }
+}
+
--- kendallsalgebra.h
+++ kendallsalgebra.h
@@ -1,6 +1,7 @@
 #ifndef KENDALLSALGEBRA_H
 #define KENDALLSALGEBRA_H
 #include "matrix.h"
+#include <vector>
 
 class KendallsAlgebra // matrix class
 {
@@ -10,6 +11,9 @@ public:
     void generateAndPrintRandomMatrix();
     int** genRandomMatrix(unsigned int height, unsigned int width);
     int* getDeterminants(unsigned int height, unsigned int width);
+
+protected:
+    void print2dVector(std::vector<std::vector<int>>);
 };
 
 
